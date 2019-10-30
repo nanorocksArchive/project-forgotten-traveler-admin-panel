3d4ad7000eecc8e3ddd56d5369799998cf94b101
nanorocks <andrejnankov@gmail.com>
2019-10-30 23:13:29 +0100

    UPD storage with main class for db, plus migration runner

---
+++ storage/db/class/Database.php
@@ -0,0 +1,80 @@
+<?php
+
+class Database
+{
+    private $type;
+
+    private $dir;
+
+    private $filename;
+
+    private $path;
+
+    public $conn;
+
+    public function __construct($type, $dir, $filename)
+    {
+        $this->type = $type;
+        $this->dir = $dir;
+        $this->filename = $filename;
+
+        if (!$this->DbExist())
+        {
+            echo $this->createDB();
+        }
+    }
+
+    public function deleteDB()
+    {
+        return unlink($this->getFilePath());
+    }
+
+    public function createDB()
+    {
+        return shell_exec('touch ' . $this->getFilePath());
+    }
+
+    public function getConfigPath()
+    {
+        return $this->path = sprintf(
+            '%s:%s/../%s',
+            $this->type,
+            $this->dir,
+            $this->filename
+        );
+    }
+
+    public function getFilePath()
+    {
+        return $this->dir . '/../' . $this->filename;
+    }
+
+    public function DbExist()
+    {
+        return file_exists($this->getFilePath());
+    }
+
+    public function setConnection()
+    {
+        $this->conn = new PDO($this->getConfigPath());
+        $this->conn->setAttribute(
+            PDO::ATTR_ERRMODE,
+            PDO::ERRMODE_EXCEPTION
+        );
+    }
+
+    public function createTable($sql, $name)
+    {
+        try{
+            $exec = $this->conn->exec($sql);
+            if(!$exec)
+            {
+                echo "Successfully create table <$name>.\n";
+            }
+
+        }catch (Exception $e)
+        {
+            echo $e;
+        }
+    }
+}
---
+++ storage/db/init.php
@@ -0,0 +1,30 @@
+<?php
+
+$config = require_once __DIR__ . '/../../src/config.php';
+
+require_once __DIR__ . '/class/Database.php';
+
+$db = new Database(
+    $config['db']['drive'],
+    __DIR__,
+    $config['db']['filename']
+);
+
+// To recreate db
+if ($db->DbExist())
+{
+    $db->deleteDB();
+    $db->createDB();
+}
+
+$db->setConnection();
+
+// Migrations here
+$users = require_once __DIR__ . '/../migration/usersMigration.php';
+$levels = require_once __DIR__ . '/../migration/levelsMIgration.php';
+$scores = require_once __DIR__ . '/../migration/scoresMigration.php';
+
+// Create tables
+$db->createTable($users, 'users');
+$db->createTable($levels, 'levels');
+$db->createTable($scores, 'scores');
