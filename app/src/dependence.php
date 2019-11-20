97e571f67f0a9a43ef214f825097d3aa70270d12
nanorocks <andrejnankov@gmail.com>
2019-11-20 01:04:23 +0100

    ADD sesion start and reorder dependencies

--- app/src/dependence.php
+++ app/src/dependence.php
@@ -1,9 +1,50 @@
 <?php
 
 // Container and dependencies
-use Tuupola\Middleware\HttpBasicAuthentication;
-
 $container = $app->getContainer();
+
+// Controllers here
+$container[\App\Controller\BaseController::class] = function ($container) {
+    return new \App\Controller\BaseController($container);
+};
+
+$container[\App\Controller\UserController::class] = function ($container) {
+    return new \App\Controller\UserController($container);
+};
+
+// Middleware here
+$container['GuestMiddleware'] = function ($container) {
+    return new \App\Middleware\GuestMiddleware($container);
+};
+
+$container['AuthMiddleware'] = function ($container) {
+    return new \App\Middleware\AuthMiddleware($container);
+};
+
+// Session here
+$container['session'] = function ($container) {
+    return new \SlimSession\Helper;
+};
+
+
+// Container for JsonWebToken Middleware
+$container['jwt'] = function ($container) {
+    return \App\Middleware\JwtMiddleware::class;
+};
+
+// Twig template engine
+$container['twig'] = function ($container) {
+    $view = new \Slim\Views\Twig($container->get('settings')['template']['path'], [
+        'cache' => false//$container->get('settings')['template']['cache'],
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $router = $container->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
+
+    return $view;
+};
 
 // Database access
 require_once __DIR__ . '/../storage/db/class/Database.php';
@@ -18,46 +59,3 @@ $container['db'] = function ($container){
     $db->setConnection();
     return $db;
 };
-
-// Controllers
-$container[\App\Controller\BaseController::class] = function ($container) {
-    return new \App\Controller\BaseController($container);
-};
-
-$container[\App\Controller\UserController::class] = function ($container) {
-    return new \App\Controller\UserController($container);
-};
-
-
-
-// Container for error logs
-//$container['logger-error'] = function($c) use ($config) {
-//    $logger = new \Monolog\Logger('request');
-//    $file_handler = new \Monolog\Handler\StreamHandler($config['logs']['error']);
-//    $logger->pushHandler($file_handler);
-//    return $logger;
-//};
-
-// Container for database logs
-//$container['logger-database'] = function($c) use ($config) {
-//    $logger = new \Monolog\Logger('database');
-//    $file_handler = new \Monolog\Handler\StreamHandler($config['logs']['database']);
-//    $logger->pushHandler($file_handler);
-//    return $logger;
-//};
-
-// Container for JsonWebToken Middleware
-$container['jwt'] = function ($container) {
-    return \App\Middleware\JwtMiddleware::class;
-};
-
-// Twig template engine
-$container['twig'] = function ($container) {
-    $loader = new \Twig\Loader\FilesystemLoader($container->get('settings')['template']['path']);
-    $twig = new \Twig\Environment($loader, [
-        'cache' => false//$container->get('settings')['template']['cache'],
-    ]);
-    return $twig;
-};
-
-
