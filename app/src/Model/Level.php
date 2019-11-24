c045b7da8f165a21415def4250028dbd9e3b314a
nanorocks <andrejnankov@gmail.com>
2019-11-24 14:41:47 +0100

    UPD user and level model

--- app/src/Model/Level.php
+++ app/src/Model/Level.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Level extends Model {
 
     protected $table = 'levels';
+
+    protected $primaryKey = 'id';
+
+    public $timestamps = false;
 }
--- app/src/Model/User.php
+++ app/src/Model/User.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class User extends Eloquent {
 
-    protected $connection = 'mysql';
-
     protected $table='users';
 
     protected $fillable = ['email', 'username'];
