e4edc12828c0843b340e7bb20fe7d7e563752651
nanorocks <andrejnankov@gmail.com>
2019-12-04 00:28:51 +0100

    UPD formating code on admin validation

--- app/src/Helper/AdminValidation.php
+++ app/src/Helper/AdminValidation.php
@@ -4,25 +4,24 @@ namespace App\Helper;
 
 use Rakit\Validation\Validator;
 
-trait AdminValidation {
+trait AdminValidation
+{
 
     public static function validateWebLogin($params)
     {
         $validator = new Validator;
 
         $validation = $validator->make($params, [
-            'email'                 => 'required|email',
-            'password'              => 'required|min:6',
+            'email' => 'required|email',
+            'password' => 'required|min:6',
         ]);
 
         $validation->validate();
 
-        if($validation->fails())
-        {
+        if ($validation->fails()) {
             $errors = $validation->errors()->toArray();
             $msgErrors = [];
-            foreach ($errors as $key => $errMsg)
-            {
+            foreach ($errors as $key => $errMsg) {
                 $msgErrors[$key] = array_values($errMsg);
             }
 
