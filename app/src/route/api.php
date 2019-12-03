d61a5dcae21433ce61303871ecd68855504514d0
nanorocks <andrejnankov@gmail.com>
2019-12-04 00:24:53 +0100

    UPD route for api

--- app/src/route/api.php
+++ app/src/route/api.php
@@ -1,28 +1,19 @@
 <?php
 
+$app->post('/api/user/register', \App\Controller\UserController::class  . ':registerUser');
+
+$app->post('/api/user/login', \App\Controller\UserController::class . ':loginUser');
+
+//$app->group('/api/user', function (){
 //
-//$app->get('/', function (Request $request, Response $response, array $args) use ($container) {
 //
-//    //var_dump($container['logger']->error('err msg'));
-//    $response->getBody()->write("Hello World");
+//    $middleware = \App\Middleware\JwtMiddleware::class;
+//    $this->post('/edit/password', \App\Controller\UserController::class . ':editApi')->add($middleware);
+//});
 //
-//    return $response;
+//$app->post('/api/forgot/password', \App\Controller\UserController::class . ':forgotPasswordApi');
 //
-//})->setName('onload')->add($container['jwt']);
-
-$app->group('/api/user', function (){
-
-    $controller = \App\Controller\UserController::class;
-    $middleware = \App\Middleware\JwtMiddleware::class;
-
-    $this->post('/register', $controller  . ':registerApi');
-    $this->post('/login', $controller . ':loginApi');
-    $this->post('/edit/password', $controller . ':editApi')->add($middleware);
-});
-
-$app->post('/api/forgot/password', \App\Controller\UserController::class . ':forgotPasswordApi');
-
-//$this->get('/api/total/users', \App\Controller\UserController::class . ':totalApi');
-
-
-
+////$this->get('/api/total/users', \App\Controller\UserController::class . ':totalApi');
+//
+//
+//
