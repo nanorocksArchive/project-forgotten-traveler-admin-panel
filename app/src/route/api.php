2262baa2c64415b6ebda18126680ad5045277eb9
nanorocks <andrejnankov@gmail.com>
2019-12-05 01:04:15 +0100

    UPD routes for api

--- app/src/route/api.php
+++ app/src/route/api.php
@@ -1,19 +1,15 @@
 <?php
 
-$app->post('/api/user/register', \App\Controller\UserController::class  . ':registerUser');
+$app->group('/api/user', function () use ($app){
 
-$app->post('/api/user/login', \App\Controller\UserController::class . ':loginUser');
+    $app->post('/register', \App\Controller\UserController::class  . ':registerUser');
 
-//$app->group('/api/user', function (){
-//
-//
-//    $middleware = \App\Middleware\JwtMiddleware::class;
-//    $this->post('/edit/password', \App\Controller\UserController::class . ':editApi')->add($middleware);
-//});
-//
-//$app->post('/api/forgot/password', \App\Controller\UserController::class . ':forgotPasswordApi');
-//
-////$this->get('/api/total/users', \App\Controller\UserController::class . ':totalApi');
-//
-//
-//
+    $app->post('/login', \App\Controller\UserController::class . ':loginUser');
+
+    $app->get('/logout', \App\Controller\UserController::class . ':logoutUser')->add(\App\Middleware\JwtMiddleware::class);
+
+    $app->put('/change/password', \App\Controller\UserController::class . ':changePassword')->add(\App\Middleware\JwtMiddleware::class);;
+
+    $app->get('/personal/info', \App\Controller\UserController::class . ':personalInfo')->add(\App\Middleware\JwtMiddleware::class);;
+
+});
