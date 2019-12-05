f358039da204da3a1e9972b385e1ecf4005f739e
nanorocks <andrejnankov@gmail.com>
2019-12-05 22:11:03 +0100

    ADD new routes for api

--- app/src/route/api.php
+++ app/src/route/api.php
@@ -6,10 +6,22 @@ $app->group('/api/user', function () use ($app){
 
     $app->post('/login', \App\Controller\UserController::class . ':loginUser');
 
-    $app->get('/logout', \App\Controller\UserController::class . ':logoutUser')->add(\App\Middleware\JwtMiddleware::class);
+    $app->get('/logout', \App\Controller\UserController::class . ':logoutUser')
+        ->add(\App\Middleware\JwtMiddleware::class);
 
-    $app->put('/change/password', \App\Controller\UserController::class . ':changePassword')->add(\App\Middleware\JwtMiddleware::class);;
+    $app->put('/change/password', \App\Controller\UserController::class . ':changePassword')
+        ->add(\App\Middleware\JwtMiddleware::class);
 
-    $app->get('/personal/info', \App\Controller\UserController::class . ':personalInfo')->add(\App\Middleware\JwtMiddleware::class);;
+    $app->get('/personal/info', \App\Controller\UserController::class . ':personalInfo')
+        ->add(\App\Middleware\JwtMiddleware::class);
+
+    $app->put('/total/time', \App\Controller\UserController::class . ':totalTime')
+        ->add(\App\Middleware\JwtMiddleware::class);
+
+    $app->get('/total/score', \App\Controller\AnalyseController::class . ':totalScoreGamePlay')
+        ->add(\App\Middleware\JwtMiddleware::class);
 
 });
+
+$app->get('/api/levels', \App\Controller\LevelController::class . ':showLevels')
+    ->add(\App\Middleware\JwtMiddleware::class);
