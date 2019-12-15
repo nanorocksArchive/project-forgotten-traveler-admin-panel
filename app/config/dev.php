76d022a75eb263b51aa5201fe79f1ac0b36aa25a
nanorocks <andrejnankov@gmail.com>
2019-12-15 21:40:14 +0100

    ADD config for prod and dev

--- app/config/dev.php
+++ app/config/dev.php
@@ -5,10 +5,10 @@ return [
     'addContentLengthHeader'=> false,
     'db' => [
         'driver' => 'mysql',
-        'host' => '94.237.88.91',
-        'database' => 'nankovin_game',
-        'username' => 'nankovin_game',
-        'password' => 'confusionsnake32;',
+        'host' => '107.189.6.84',
+        'database' => 'nankovmk_ftraveler_dev',
+        'username' => 'nankovmk_ftraveler',
+        'password' => 'iOYYjW!0jaR_nK+tKi',
         'charset'   => 'utf8',
         'collation' => 'utf8_unicode_ci',
         'prefix'    => '',
@@ -29,5 +29,14 @@ return [
         'type' => 'twig',
         'path' => __DIR__ . '/../src/View/',
         'cache' => __DIR__ . '/../logs/'
+    ],
+    'mail' => [
+        'senderName' => 'Forgotten Traveler',
+        'senderEmail' => 'mail@ftadmin.php.mk',
+        'customId' => 'thisIsNewMobileGame32;',
+        'key' => 'd813129af503e903ff40fae06f65e57f',
+        'secret' => '18c50ea2fb82dde307d66a0ef3460604',
+        'call' => true,
+        'version' => ['version' => 'v3.1']
     ]
 ];
