de43f5c0f488aeefbfa7cd664ffa3f4610af195f
nanorocks <andrejnankov@gmail.com>
2019-12-28 14:37:58 +0100

    UPD base controller add user controller, add store password method

--- app/src/Controller/BaseController.php
+++ app/src/Controller/BaseController.php
@@ -6,8 +6,15 @@ use Psr\Container\ContainerInterface;
 
 class BaseController{
 
+    /**
+     * @var ContainerInterface
+     */
     public $container;
 
+    /**
+     * BaseController constructor.
+     * @param ContainerInterface $container
+     */
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -9,6 +9,7 @@ use Gamegos\JWS\JWS;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use App\Model\User;
+use TheSeer\Tokenizer\Exception;
 
 class UserController extends BaseController
 {
@@ -34,8 +35,7 @@ class UserController extends BaseController
 
         $errors = UserValidation::validateRegistration($params);
 
-        if (!empty($errors))
-        {
+        if (!empty($errors)) {
             return $response->withJson([
                 'message' => "Invalid data",
                 'err' => $errors,
@@ -45,14 +45,13 @@ class UserController extends BaseController
 
         $user = new User();
         $user->username = $username;
-        $user->password =  md5($password);
+        $user->password = md5($password);
         $user->email = $email;
         $user->total_time = $totalTime;
         $user->activation = 0;
         $success = $user->save();
 
-        if(!$success)
-        {
+        if (!$success) {
             return $response->withJson([
                 'message' => "There is a CONFLICT",
                 'code' => 409
@@ -82,8 +81,7 @@ class UserController extends BaseController
 
         $errors = UserValidation::validateLogin($params);
 
-        if (!empty($errors))
-        {
+        if (!empty($errors)) {
             return $response->withJson([
                 'message' => "Invalid data",
                 'err' => $errors,
@@ -92,7 +90,7 @@ class UserController extends BaseController
         }
 
         $user = User::where('username', '=', $username)->where('password', '=', md5($password))->first();
-        if(!$user){
+        if (!$user) {
             return $response->withJson([
                 'message' => "Invalid username or password",
                 'code' => 200
@@ -114,7 +112,7 @@ class UserController extends BaseController
 
         $jws = new JWS();
         $token = $jws->encode($headers, $payload, $key);
-        $token = sprintf("%s %s", $config['access'] , $token);
+        $token = sprintf("%s %s", $config['access'], $token);
 
         return $response->withJson([
             'message' => "Success",
@@ -143,8 +141,7 @@ class UserController extends BaseController
         // validate new password
         $errors = UserValidation::validateNewPassword($params);
 
-        if (!empty($errors))
-        {
+        if (!empty($errors)) {
             return $response->withJson([
                 'message' => "Invalid data",
                 'err' => $errors,
@@ -153,7 +150,7 @@ class UserController extends BaseController
         }
 
         $user = User::where('username', '=', $username)->where('password', '=', $oldPassword)->first();
-        if(!$user){
+        if (!$user) {
             return $response->withJson([
                 'message' => "Invalid token",
                 'code' => 200
@@ -163,8 +160,7 @@ class UserController extends BaseController
         $user->password = md5($newPassword);
         $success = $user->save();
 
-        if($success)
-        {
+        if ($success) {
             return $response->withJson([
                 'message' => "Your password is changed",
                 'report' => "Delete token form client side and logout user",
@@ -211,7 +207,7 @@ class UserController extends BaseController
         $password = $data['payload']['password'];
 
         $user = User::where('username', '=', $username)->where('password', '=', $password)->first();
-        if(!$user){
+        if (!$user) {
             return $response->withJson([
                 'message' => "Invalid token",
                 'code' => 200
@@ -245,7 +241,7 @@ class UserController extends BaseController
         $password = $data['payload']['password'];
 
         $user = User::where('username', '=', $username)->where('password', '=', $password)->first();
-        if(!$user){
+        if (!$user) {
             return $response->withJson([
                 'message' => "Invalid token",
                 'code' => 200
@@ -258,8 +254,7 @@ class UserController extends BaseController
         // validate total time
         $errors = UserValidation::validateTotalTime($params);
 
-        if (!empty($errors))
-        {
+        if (!empty($errors)) {
             return $response->withJson([
                 'message' => "Invalid data",
                 'err' => $errors,
@@ -270,8 +265,7 @@ class UserController extends BaseController
         $user->total_time = $totalTime;
         $success = $user->save();
 
-        if($success)
-        {
+        if ($success) {
             return $response->withJson([
                 'message' => "You update total time",
                 'code' => 200
@@ -299,8 +293,7 @@ class UserController extends BaseController
 
         // validate email
         $errors = UserValidation::validateEmail($params);
-        if (!empty($errors))
-        {
+        if (!empty($errors)) {
             return $response->withJson([
                 'message' => "Invalid data",
                 'err' => $errors,
@@ -308,10 +301,10 @@ class UserController extends BaseController
             ], 409);
         }
 
-        $user = User::where('email', '=' , $email)->first();
+        $user = User::where('email', '=', $email)->first();
         $username = $user->username;
 
-        $http = isset( $_SERVER['HTTPS'] ) ? 'https://' : 'http://';
+        $http = isset($_SERVER['HTTPS']) ? 'https://' : 'http://';
         $hash = bin2hex(openssl_random_pseudo_bytes(16));
         $resetCode = sprintf('%s%s/password/%s',
             $http,
@@ -320,8 +313,7 @@ class UserController extends BaseController
         );
 
         $fp = ForgotPassword::find($user->id);
-        if($fp == null)
-        {
+        if ($fp == null) {
             $fp = new ForgotPassword();
         }
 
@@ -330,13 +322,13 @@ class UserController extends BaseController
         $fp->save();
 
         $success = MailSender::send(
+            $this->container,
             $username,
             $email,
             $resetCode
         );
 
-        if($success)
-        {
+        if ($success) {
             return $response->withJson([
                 'message' => "Check your email",
                 'code' => 200
@@ -361,23 +353,95 @@ class UserController extends BaseController
     {
         $token = $args['token'];
         $ft = ForgotPassword::where('reset', '=', $token)->first();
-        $expire = false;
-        if (!$ft) {
+        $expire = 0;
+        $tokenMsg = 'Invalid token. Try again.';
+        if ($ft == null) {
             return $this->container['twig']->render($response, 'user/forgot-password.php.twig', [
-                'tokenExpire' => $expire
+                'tokenExpire' => $expire,
+                'token' => $token,
+                'tokenMsg' => $tokenMsg
             ]);
         }
 
         $forgotPassword = $ft->toArray();
         $updatedDate = date('Y-m-d H:m:s', strtotime($forgotPassword['updated_at'] . ' +1 day'));
         $today = date('Y-m-d H:m:s');
-        if(strtotime($updatedDate) <= strtotime($today))
-        {
+        $expire = 1;
+
+        if (strtotime($updatedDate) <= strtotime($today)) {
             $ft->delete();
-            $expire = true;
+            $expire = 0;
+            $tokenMsg = 'Token expired. Try again.';
         }
+
         return $this->container['twig']->render($response, 'user/forgot-password.php.twig', [
-            'tokenExpire' => $expire
+            'tokenExpire' => $expire,
+            'token' => $token,
+            'tokenMsg' => $tokenMsg
         ]);
+    }
+
+    /**
+     * Save password on submit forgot password by user
+     *
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return mixed
+     * @throws \Exception
+     */
+    public function submitPassword(RequestInterface $request, ResponseInterface $response, $args = [])
+    {
+        $token = $args['token'];
+        $ft = ForgotPassword::where('reset', '=', $token)->first();
+        $expire = 0;
+        $tokenMsg = 'Invalid token. Try again.';
+        if ($ft == null) {
+            return $this->container['twig']->render($response, 'user/forgot-password.php.twig', [
+                'tokenExpire' => $expire,
+                'token' => $token,
+                'tokenMsg' => $tokenMsg
+            ]);
+        }
+
+        try {
+            $post = $request->getParsedBody();
+            $errors = UserValidation::validateNewPassword($post);
+            if (!empty($errors)) {
+                $expire = 1;
+                return $this->container['twig']->render($response, 'user/forgot-password.php.twig', [
+                    'tokenExpire' => $expire,
+                    'token' => $token,
+                    'tokenMsg' => $tokenMsg,
+                    'err' => $errors
+                ]);
+            }
+
+            $ftArray = $ft->toArray();
+            $user = User::find($ftArray['id_user'])->first();
+            $user->password = md5($post['new-password']);
+            $success = $user->save();
+            if(!$success)
+            {
+                throw new \Exception('Password not saved.');
+            }
+            $ft->delete();
+
+        } catch (Exception $e) {
+            return $this->container['twig']->render($response, 'user/forgot-password.php.twig', [
+                'tokenExpire' => $expire,
+                'token' => $token,
+                'tokenMsg' => $tokenMsg
+            ]);
+        }
+        $expire = 0;
+        $successMsg = 'Password changed. Continue to play.';
+        return $this->container['twig']->render($response, 'user/forgot-password.php.twig', [
+            'tokenExpire' => $expire,
+            'token' => $token,
+            'tokenMsg' => $tokenMsg,
+            'successMsg' => $successMsg
+        ]);
+
     }
 }
