7de5c848c0bae82670ea8b3f410015e7da496cd1
nanorocks <andrejnankov@gmail.com>
2019-12-28 14:32:15 +0100

    UPD routes view

--- app/src/route/web.php
+++ app/src/route/web.php
@@ -8,17 +8,26 @@ $app->post('/login', \App\Controller\AdminController::class . ':loginWeb')
 
 $app->group('', function (){
 
-    $this->get('/dashboard', \App\Controller\AdminController::class . ':dashboard')->setName('dashboard');
+    $this->get('/dashboard', \App\Controller\AdminController::class . ':dashboard')
+        ->setName('dashboard');
 
-    $this->get('/new/level', \App\Controller\AdminController::class . ':newLevelWeb')->setName('new.level');
-    $this->post('/store/new/level', \App\Controller\AdminController::class . ':storeNewLevelWeb')->setName('store.level');
+    $this->get('/new/level', \App\Controller\AdminController::class . ':newLevelWeb')
+        ->setName('new.level');
+    $this->post('/store/new/level', \App\Controller\AdminController::class . ':storeNewLevelWeb')
+        ->setName('store.level');
 
-    $this->get('/edit/level/{id}', \App\Controller\AdminController::class . ':editLevelWeb')->setName('edit.level');
-    $this->post('/update/level/{id}', \App\Controller\AdminController::class . ':updateLevelWeb')->setName('update.level');
-    $this->get('/delete/level/{id}', \App\Controller\AdminController::class . ':deleteLevelWeb')->setName('delete.level');
+    $this->get('/edit/level/{id}', \App\Controller\AdminController::class . ':editLevelWeb')
+        ->setName('edit.level');
+    $this->post('/update/level/{id}', \App\Controller\AdminController::class . ':updateLevelWeb')
+        ->setName('update.level');
+    $this->get('/delete/level/{id}', \App\Controller\AdminController::class . ':deleteLevelWeb')
+        ->setName('delete.level');
 
-    $this->get('/logout', \App\Controller\AdminController::class . ':logout')->setName('logout.page');
+    $this->get('/logout', \App\Controller\AdminController::class . ':logout')
+        ->setName('logout.page');
 
 })->add(\App\Middleware\AuthMiddleware::class);
 
 $app->get('/password/{token}', \App\Controller\UserController::class . ':enterPassword');
+$app->post('/password/{token}', \App\Controller\UserController::class . ':submitPassword')
+    ->setName('submit.password');
